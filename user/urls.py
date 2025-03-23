94f271c93b4542de1c7aa0b0d2d6610627ad527e
unknown <ederhenriquevicentejust18@gmail.com>
2025-03-23 13:53:31 -0300

    Erro de rota

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('forgot-password/', views.forgotPassword, name='forgot_password'),
-    path('reset-password/<str:token>/', views.resetPassword, name='reset_password'),
 ]
